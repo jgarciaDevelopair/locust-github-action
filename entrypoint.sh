c8596f290eebcbd64c916048083a5561d1944e2e
jgarciaDevelopair <49644671+jgarciaDevelopair@users.noreply.github.com>
2022-02-24 11:37:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,10 @@ else
     file="/github/workspace/$INPUT_LOCUSTFILE"
 fi
 
-locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+#locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+locust -f $file --csv=results --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+
+for filename in *.csv; do
+    [ -e "$filename" ] || continue
+    s3 cp "$filename" "S3_BUCKET/"
+done
