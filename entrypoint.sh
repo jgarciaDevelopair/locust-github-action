70e95be24c65f41288b7a7788dab660aee254348
jgarciaDevelopair <49644671+jgarciaDevelopair@users.noreply.github.com>
2022-02-24 10:49:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-if [ -z "$INPUT_LOCUSTFILE" ]
+if[ -z"$INPUT_LOCUSTFILE"]
 then
     file="/locustfile.py"
 else
     file="/github/workspace/$INPUT_LOCUSTFILE"
 fi
 
-locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+locust -f $file--headless -u $INPUT_USERS-r $INPUT_RATE--run-time $INPUT_RUNTIME-H $INPUT_URL --csv="$2_$(date +%F_%T)"
+for filename in *.csv; do
+    [ -e "$filename" ] || continue
+    #Upload to S3
+    s3 cp "$filename" "S3_BUCKET/"
+done
